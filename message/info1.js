db9ce03616a3f1ca903952dce44947160c1555ff
KrisnaZx <achmadkrisna345@gmail.com>
2021-11-26 03:32:04 +0000

    first commit

---
+++ message/info1.js
@@ -0,0 +1,17 @@
+exports.bot = (gender,fake,nomerOwner)=> {
+return`
+*╭─▸*「 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧𝗭 」
+*│*
+*│▸* Nama : ${fake}
+*│▸* Gender : ${gender}
+*│▸* Hoby : -
+*│▸* Owner : ${nomerOwner}
+*│▸* Tgl pembuatan : 20-11-2021
+*│▸* Tgl release : 25-11-2021
+*│▸* Image : Ichiyy-Botz
+*│▸* Voice : Ichiyy Kawai
+*│*
+*╰────────────[*`
+}
+
+
